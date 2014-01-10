f5401bcfe0e87a95ca2897af91348f21a9d1738f
elf <elf198012@gmail.com>
2014-01-10 11:58:42 +0800

    redesign list

--- include/List.h
+++ include/List.h
@@ -5,51 +5,136 @@
 
 //Double linked list
 
-typedef struct struct_List_Node{
- struct struct_List_Node *pNext;
- struct struct_List_Node *pPrev;
-}List_Node;
+//Node
+typedef struct struct_ListNode ListNode;
+typedef struct struct_ListNode{
+ ListNode *pNext;
+ ListNode *pPrev;
+};
 
-static inline void List_Initialize(List_Node *pEntry){
- pEntry->pNext = pEntry;
- pEntry->pPrev = pEntry;
+static inline void ListNode_LinkPrev(ListNode *pNode, ListNode *pPrev){
+ pNode->pPrev = pPrev;
+ pPrev->pNext = pNode;
 }
 
-static inline Bool List_Empty(List_Node *pEntry){
- return pEntry->pNext == pEntry;
+static inline void ListNode_LinkNext(ListNode *pNode, ListNode *pNext){
+ pNode->pNext = pNext;
+ pNext->pPrev = pNode;
 }
 
-static inline Bool List_NotEmpty(List_Node *pEntry){
- return pEntry->pNext != pEntry;
+static inline void ListNode_Link(ListNode *pNode, ListNode *pPrev, ListNode *pNext){
+ ListNode_LinkPrev(pNode, pPrev);
+ ListNode_LinkNext(pNode, pNext);
 }
 
-static inline void List_Remove(List_Node *pNode){
+static inline void ListNode_Unlink(ListNode *pNode){
  pNode->pPrev->pNext = pNode->pNext;
  pNode->pNext->pPrev = pNode->pPrev;
 }
 
-static inline void List_Insert(List_Node *pPosition, List_Node *pNode){
- pNode->pPrev = pPosition->pPrev;
- pNode->pNext = pPosition;
-
- pPosition->pPrev->pNext = pNode;
- pPosition->pPrev = pNode;
+static inline void ListNode_Reset(ListNode *pNode){
+ pNode->pPrev = pNode->pNext = pNode;
 }
 
-static inline void List_Append(List_Node *pTarget, List_Node *pNode){
- pNode->pPrev = pTarget;
- pNode->pNext = pTarget->pNext;
-
- pTarget->pNext->pPrev = pNode;
- pTarget->pNext = pNode;
+static inline Bool ListNode_Alone(ListNode *pNode){
+ return pNode->pNext == pNode;
 }
 
-static inline List_Node *List_Pop(List_Node *pEntry){
- List_Node *pNode = pEntry->pNext;
- List_Remove(pNode);
+static inline Bool ListNode_NotAlone(ListNode *pNode){
+ return pNode->pNext != pNode;
+}
+
+static inline void ListNode_MoveBuddiesTo(ListNode *pNode, ListNode *pTargetNode){
+ ListNode_LinkPrev(pNode->pNext, pTargetNode->pPrev);
+ ListNode_LinkNext(pNode->pPrev, pTargetNode);
+ ListNode_Reset(pNode);
+}
+
+static inline void ListNode_MoveBuddiesToFront(ListNode *pNode, ListNode *pTargetNode){
+ ListNode_LinkPrev(pNode->pNext, pTargetNode);
+ ListNode_LinkNext(pNode->pPrev, pTargetNode->pNext);
+ ListNode_Reset(pNode);
+}
+
+static inline void ListNode_SwapBuddies(ListNode *pNode, ListNode *pTargetNode){
+ ListNode tmp;
+ ListNode_Reset(&tmp);
+ ListNode_MoveBuddiesTo(pNode, &tmp);
+ ListNode_MoveBuddiesTo(pTargetNode, pNode);
+ ListNode_MoveBuddiesTo(&tmp, pTargetNode);
+}
+//List
+//If you do not want counting, use ListNode directly.
+typedef struct{
+ Node entry;
+ size_t nCount;
+}List;
+
+static inline void List_Reset(List *pList){
+ ListNode_Reset(&pList->entry);
+ pList->nCount = 0;
+}
+
+static inline Bool List_Empty(const List *pList){
+ return ListNode_Alone(&pList->entry);
+}
+
+static inline Bool List_NotEmpty(const List *pList){
+ return ListNode_NotAlone(&pList->entry);
+}
+
+static inline void List_Insert(List *pList, ListNode *pNode, ListNode *pPrev, ListNode *pNext){
+ ListNode_Link(pNode, pPrev, pNext);
+ ++pList->nCount;
+}
+
+static inline void List_Remove(List *pList, ListNode *pNode){
+ ListNode_Unlink(pNode);
+ --pList->nCount;
+}
+
+//push back
+static inline void List_Push(List *pList, ListNode *pNode){
+ ListNode_Link(pNode, pList->entry.pPrev, &pList->entry);
+ ++pList->nCount;
+}
+
+//pop front
+static inline ListNode *List_Pop(List *pList){
+ ListNode *pNode = pList->entry.pNext;
+ ListNode_Unlink(pNode);
+ --pList->nCount;
  return pNode;
 }
 
-static inline void List_Push(List_Node *pEntry, List_Node *pNode){
- List_Append(pEntry, pNode);
+static inline void List_PushFront(List *pList, ListNode *pNode){
+ ListNode_Link(pNode, &pList->entry, pList->entry.pNext);
+ ++pList->nCount;
+}
+
+static inline ListNode *List_PopBack(List *pList){
+ ListNode *pNode = pList->entry.pPrev;
+ ListNode_Unlink(pNode);
+ --pList->nCount;
+ return pNode;
+}
+
+//move to back
+static inline void List_MoveTo(List *pList, List *pTargetList){
+ ListNode_MoveBuddiesTo(&pList->entry, &pTargetList->entry);
+ pTargetList->nCount += pList->nCount;
+ pList->nCount = 0;
+}
+
+static inline void List_MoveToFront(List *pList, List *pTargetList){
+ ListNode_MoveBuddiesToFront(&pList->entry, &pTargetList->entry);
+ pTargetList->nCount += pList->nCount;
+ pList->nCount = 0;
+}
+
+static inline void List_Swap(List *pList, List *pTargetList){
+ ListNode_SwapBuddies(&pList->entry, &pTargetList->entry);
+ size_t nTmp = pList->nCount;
+ pList->nCount = pTargetList->nCount;
+ pTargetList->nCount = nTmp;
 }
