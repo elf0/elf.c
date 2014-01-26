86fbead1319422614b73eeaad520bfb13153a7f7
elf <elf198012@gmail.com>
2014-01-26 11:07:00 +0800

    Redesign SharedObject

--- include/SharedObject.h
+++ include/SharedObject.h
@@ -4,47 +4,26 @@
 //EMail: elf198012@gmail.com
 
 template<typename T>
-class SharedObject{
-public:
- static SharedObject *New(){
-  return new SharedObject();
- }
-
- void Reference(){
-  ++_nCount;
-//  fprintf(stderr, "Reference: %lu\n", _nCount);
- }
-
- void Dereference(){
-//  fprintf(stderr, "Dereference: %lu\n", _nCount);
-  if(--_nCount == 0)
-   delete this;
- }
-
- T &operator*(){
-  return _object;
- }
-
-private:
- SharedObject(){}
- ~SharedObject(){}
-
- T _object;
- size_t _nCount = 1;
-};
+class SharedObject;
 
 template<typename T>
 class ObjectUser{
-protected:
+//protected:
+public:
  ObjectUser(){};
- ObjectUser(SharedObject<T> *pObject): _pObject(pObject){}
+
+ ObjectUser(SharedObject<T> *pObject): _pObject(pObject){
+  fprintf(stderr, "ObjectUser(SharedObject<T> *pObject)\n");
+ }
 
  ObjectUser(const ObjectUser &other)
-  : ObjectUser(other._pObject){
+  : _pObject(other._pObject){
+  fprintf(stderr, "Copy constructor\n");
   Reference();
  }
 
  ObjectUser &operator=(const ObjectUser &other){
+  fprintf(stderr, "operator=()\n");
   if(_pObject == other._pObject)
    return *this;
 
@@ -57,7 +36,7 @@ protected:
  ~ObjectUser(){Dereference();}
 
  T &Object()const{
-  return **_pObject;
+  return _pObject->Object();
  }
 
 private:
@@ -71,4 +50,34 @@ private:
    _pObject->Dereference();
  }
  SharedObject<T> *_pObject = nullptr;
+};
+
+template<typename T>
+class SharedObject{
+public:
+ static ObjectUser<T> New(const T &object){
+  return ObjectUser<T>(new SharedObject(object));
+ }
+
+ void Reference(){
+  ++_nCount;
+  //fprintf(stderr, "Reference: %lu\n", _nCount);
+ }
+
+ void Dereference(){
+  //fprintf(stderr, "Dereference: %lu\n", _nCount);
+  if(--_nCount == 0)
+   delete this;
+ }
+
+ T &Object(){
+  return _object;
+ }
+
+private:
+ SharedObject(const T &object): _object(object){}
+ ~SharedObject(){}
+
+ T _object;
+ size_t _nCount = 1;
 };
