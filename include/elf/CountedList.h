9d64a5e5e496c045e77975960f712f0f6011bb98
elf <elf198012@gmail.com>
2014-02-19 15:21:53 +0800

    Add List's Intialize function

--- include/elf/CountedList.h
+++ include/elf/CountedList.h
@@ -10,9 +10,13 @@ typedef struct{
     MU nCount;
 }elf_CountedList;
 
-static inline void elf_CountedList_Reset (elf_CountedList *pList){
+static inline void elf_CountedList_Reset(elf_CountedList *pList){
     elf_List_Reset((elf_List*)pList);
     pList->nCount = 0;
+}
+
+static inline void elf_CountedList_Initialize(elf_CountedList *pList){
+    elf_CountedList_Reset(pList);
 }
 
 static inline Bool elf_CountedList_Empty(const elf_CountedList *pList){
@@ -28,25 +32,25 @@ static inline void elf_CountedList_Insert(elf_CountedList *pList, elf_DoubleNode
     ++pList->nCount;
 }
 
-static inline void elf_CountedList_Remove (elf_CountedList *pList, elf_DoubleNode *pNode){
+static inline void elf_CountedList_Remove(elf_CountedList *pList, elf_DoubleNode *pNode){
     elf_List_Remove(pNode);
     --pList->nCount;
 }
 
 //push back
-static inline void elf_CountedList_Push (elf_CountedList *pList, elf_DoubleNode *pNode){
+static inline void elf_CountedList_Push(elf_CountedList *pList, elf_DoubleNode *pNode){
     elf_List_Push((elf_List*)pList, pNode);
     ++pList->nCount;
 }
 
 //pop front
-static inline elf_DoubleNode *elf_CountedList_Pop (elf_CountedList *pList){
+static inline elf_DoubleNode *elf_CountedList_Pop(elf_CountedList *pList){
     elf_DoubleNode *pNode = elf_List_Pop((elf_List*)pList);
     --pList->nCount;
     return pNode;
 }
 
-static inline void elf_CountedList_PushFront (elf_CountedList *pList, elf_DoubleNode *pNode){
+static inline void elf_CountedList_PushFront(elf_CountedList *pList, elf_DoubleNode *pNode){
     elf_List_PushFront((elf_List*)pList, pNode);
     ++pList->nCount;
 }
@@ -58,21 +62,22 @@ static inline elf_DoubleNode *CountedList_PopBack(elf_CountedList *pList){
 }
 
 //move to back
-static inline void elf_CountedList_MoveTo (elf_CountedList *pList, elf_CountedList *pTargetList){
+static inline void elf_CountedList_MoveTo(elf_CountedList *pList, elf_CountedList *pTargetList){
     elf_List_MoveTo((elf_List*)pList, (elf_List*)pTargetList);
     pTargetList->nCount += pList->nCount;
     pList->nCount = 0;
 }
 
-static inline void elf_CountedList_MoveToFront (elf_CountedList *pList, elf_CountedList *pTargetList){
+static inline void elf_CountedList_MoveToFront(elf_CountedList *pList, elf_CountedList *pTargetList){
     elf_List_MoveToFront((elf_List*)pList, (elf_List*)pTargetList);
     pTargetList->nCount += pList->nCount;
     pList->nCount = 0;
 }
 
-static inline void elf_CountedList_Swap (elf_CountedList *pList, elf_CountedList *pTargetList){
+static inline void elf_CountedList_Swap(elf_CountedList *pList, elf_CountedList *pTargetList){
     elf_List_Swap ((elf_List*)pList, (elf_List*)pTargetList);
     MU nTmp = pList->nCount;
     pList->nCount = pTargetList->nCount;
     pTargetList->nCount = nTmp;
 }
+
--- include/elf/List.h
+++ include/elf/List.h
@@ -9,40 +9,44 @@ typedef struct{
     elf_DoubleNode entry;
 }elf_List;
 
-static inline void elf_List_Reset (elf_List *pList){
-    elf_DoubleNode_Reset(&pList->entry);
+static inline void elf_List_Reset(elf_List *pList){
+    elf_DoubleNode_Reset((elf_DoubleNode*)pList);
+}
+
+static inline void elf_List_Initialize(elf_List *pList){
+    elf_List_Reset(pList);
 }
 
 static inline Bool elf_List_Empty(const elf_List *pList){
-    return elf_DoubleNode_Alone(&pList->entry);
+    return elf_DoubleNode_Alone((elf_DoubleNode*)pList);
 }
 
 static inline Bool elf_List_NotEmpty(const elf_List *pList){
-    return elf_DoubleNode_NotAlone(&pList->entry);
+    return elf_DoubleNode_NotAlone((elf_DoubleNode*)pList);
 }
 
-static inline void elf_List_Insert (elf_DoubleNode *pNode, elf_DoubleNode *pPrev, elf_DoubleNode *pNext){
+static inline void elf_List_Insert(elf_DoubleNode *pNode, elf_DoubleNode *pPrev, elf_DoubleNode *pNext){
     elf_DoubleNode_Link(pNode, pPrev, pNext);
 }
 
-static inline void elf_List_Remove (elf_DoubleNode *pNode){
+static inline void elf_List_Remove(elf_DoubleNode *pNode){
     elf_DoubleNode_Unlink(pNode);
 }
 
 //push back
-static inline void elf_List_Push (elf_List *pList, elf_DoubleNode *pNode){
-    elf_DoubleNode_Link(pNode, pList->entry.pPrev, &pList->entry);
+static inline void elf_List_Push(elf_List *pList, elf_DoubleNode *pNode){
+    elf_DoubleNode_Link(pNode, pList->entry.pPrev, (elf_DoubleNode*)pList);
 }
 
 //pop front
-static inline elf_DoubleNode *elf_List_Pop (elf_List *pList){
+static inline elf_DoubleNode *elf_List_Pop(elf_List *pList){
     elf_DoubleNode *pNode = pList->entry.pNext;
     elf_DoubleNode_Unlink(pNode);
     return pNode;
 }
 
-static inline void elf_List_PushFront (elf_List *pList, elf_DoubleNode *pNode){
-    elf_DoubleNode_Link(pNode, &pList->entry, pList->entry.pNext);
+static inline void elf_List_PushFront(elf_List *pList, elf_DoubleNode *pNode){
+    elf_DoubleNode_Link(pNode, (elf_DoubleNode*)pList, pList->entry.pNext);
 }
 
 static inline elf_DoubleNode *elf_List_PopBack (elf_List *pList){
@@ -52,14 +56,15 @@ static inline elf_DoubleNode *elf_List_PopBack (elf_List *pList){
 }
 
 //move to back
-static inline void elf_List_MoveTo (elf_List *pList, elf_List *pTargetList){
-    elf_DoubleNode_MoveBuddiesTo(&pList->entry, &pTargetList->entry);
+static inline void elf_List_MoveTo(elf_List *pList, elf_List *pTargetList){
+    elf_DoubleNode_MoveBuddiesTo((elf_DoubleNode*)pList, (elf_DoubleNode*)pTargetList);
 }
 
-static inline void elf_List_MoveToFront (elf_List *pList, elf_List *pTargetList){
-    elf_DoubleNode_MoveBuddiesToFront(&pList->entry, &pTargetList->entry);
+static inline void elf_List_MoveToFront(elf_List *pList, elf_List *pTargetList){
+    elf_DoubleNode_MoveBuddiesToFront((elf_DoubleNode*)pList, (elf_DoubleNode*)pTargetList);
 }
 
-static inline void elf_List_Swap (elf_List *pList, elf_List *pTargetList){
-    elf_DoubleNode_SwapBuddies(&pList->entry, &pTargetList->entry);
+static inline void elf_List_Swap(elf_List *pList, elf_List *pTargetList){
+    elf_DoubleNode_SwapBuddies((elf_DoubleNode*)pList, (elf_DoubleNode*)pTargetList);
 }
+
