fac7ceaf09250a3bd36b816f4717173e4c20ba41
elf <elf198012@gmail.com>
2014-02-19 14:33:11 +0800

    Add MU, Mi

--- include/Type.h
+++ include/Type.h
@@ -6,15 +6,20 @@
 typedef void* Pointer;
 typedef unsigned char      Byte;
 typedef unsigned char      Char;
+
+typedef unsigned long      MU;
 typedef unsigned char      U8;
 typedef unsigned short     U16;
-typedef unsigned int         U32;
+typedef unsigned int       U32;
 typedef unsigned long long U64;
+
+typedef signed long        MI;
 typedef signed char        I8;
 typedef signed short       I16;
-typedef signed int           I32;
-typedef signed long long I64;
-typedef float                 F32;
+typedef signed int         I32;
+typedef signed long long  I64;
+
+typedef float              F32;
 typedef double             F64;
 
 typedef unsigned char Bool;
@@ -22,3 +27,4 @@ typedef unsigned char Bool;
 #define true  1
 
 #define null  0
+
