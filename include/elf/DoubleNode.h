eb9fe41d54c56603b27a9a3864ed5cc23c4760ac
elf <elf198012@gmail.com>
2014-08-19 09:42:03 +0800

    Modify DoubleNode.h

--- include/elf/DoubleNode.h
+++ include/elf/DoubleNode.h
@@ -5,62 +5,62 @@
 //Author: elf
 //EMail: elf198012@gmail.com
 
-typedef struct elf_struct_DoubleNode elf_DoubleNode;
-struct elf_struct_DoubleNode{
-    elf_DoubleNode *pNext;
-    elf_DoubleNode *pPrev;
+typedef struct struct_DoubleNode DoubleNode;
+struct struct_DoubleNode{
+    DoubleNode *pNext;
+    DoubleNode *pPrev;
 };
 
-static inline void elf_DoubleNode_LinkPrev(elf_DoubleNode *pNode, elf_DoubleNode *pPrev){
+static inline void DoubleNode_LinkPrev(DoubleNode *pNode, DoubleNode *pPrev){
     pNode->pPrev = pPrev;
     pPrev->pNext = pNode;
 }
 
-static inline void elf_DoubleNode_LinkNext(elf_DoubleNode *pNode, elf_DoubleNode *pNext){
+static inline void DoubleNode_LinkNext(DoubleNode *pNode, DoubleNode *pNext){
     pNode->pNext = pNext;
     pNext->pPrev = pNode;
 }
 
-static inline void elf_DoubleNode_Link(elf_DoubleNode *pNode, elf_DoubleNode *pPrev, elf_DoubleNode *pNext){
-    elf_DoubleNode_LinkPrev(pNode, pPrev);
-    elf_DoubleNode_LinkNext(pNode, pNext);
+static inline void DoubleNode_Link(DoubleNode *pNode, DoubleNode *pPrev, DoubleNode *pNext){
+    DoubleNode_LinkPrev(pNode, pPrev);
+    DoubleNode_LinkNext(pNode, pNext);
 }
 
-static inline void elf_DoubleNode_Unlink(elf_DoubleNode *pNode){
+static inline void DoubleNode_Unlink(DoubleNode *pNode){
     pNode->pPrev->pNext = pNode->pNext;
     pNode->pNext->pPrev = pNode->pPrev;
 }
 
-static inline void elf_DoubleNode_Reset(elf_DoubleNode *pNode){
+static inline void DoubleNode_Reset(DoubleNode *pNode){
     pNode->pPrev = pNode->pNext = pNode;
 }
 
-static inline Bool elf_DoubleNode_Alone(const elf_DoubleNode *pNode){
+static inline Bool DoubleNode_Alone(const DoubleNode *pNode){
     return pNode->pNext == pNode;
 }
 
-static inline Bool elf_DoubleNode_NotAlone(const elf_DoubleNode *pNode){
+static inline Bool DoubleNode_NotAlone(const DoubleNode *pNode){
     return pNode->pNext != pNode;
 }
 
-static inline void elf_DoubleNode_MoveBuddiesTo(elf_DoubleNode *pNode, elf_DoubleNode *pTargetNode){
-    elf_DoubleNode_LinkPrev(pNode->pNext, pTargetNode->pPrev);
-    elf_DoubleNode_LinkNext(pNode->pPrev, pTargetNode);
-    elf_DoubleNode_Reset(pNode);
+static inline void DoubleNode_MoveBuddiesTo(DoubleNode *pNode, DoubleNode *pTargetNode){
+    DoubleNode_LinkPrev(pNode->pNext, pTargetNode->pPrev);
+    DoubleNode_LinkNext(pNode->pPrev, pTargetNode);
+    DoubleNode_Reset(pNode);
 }
 
-static inline void elf_DoubleNode_MoveBuddiesToFront(elf_DoubleNode *pNode, elf_DoubleNode *pTargetNode){
-    elf_DoubleNode_LinkPrev(pNode->pNext, pTargetNode);
-    elf_DoubleNode_LinkNext(pNode->pPrev, pTargetNode->pNext);
-    elf_DoubleNode_Reset(pNode);
+static inline void DoubleNode_MoveBuddiesToFront(DoubleNode *pNode, DoubleNode *pTargetNode){
+    DoubleNode_LinkPrev(pNode->pNext, pTargetNode);
+    DoubleNode_LinkNext(pNode->pPrev, pTargetNode->pNext);
+    DoubleNode_Reset(pNode);
 }
 
-static inline void elf_DoubleNode_SwapBuddies(elf_DoubleNode *pNode, elf_DoubleNode *pTargetNode){
-    elf_DoubleNode tmp;
-    elf_DoubleNode_Reset(&tmp);
-    elf_DoubleNode_MoveBuddiesTo(pNode, &tmp);
-    elf_DoubleNode_MoveBuddiesTo(pTargetNode, pNode);
-    elf_DoubleNode_MoveBuddiesTo(&tmp, pTargetNode);
+static inline void DoubleNode_SwapBuddies(DoubleNode *pNode, DoubleNode *pTargetNode){
+    DoubleNode tmp;
+    DoubleNode_Reset(&tmp);
+    DoubleNode_MoveBuddiesTo(pNode, &tmp);
+    DoubleNode_MoveBuddiesTo(pTargetNode, pNode);
+    DoubleNode_MoveBuddiesTo(&tmp, pTargetNode);
 }
 
 #endif //DOUBLENODE_H
