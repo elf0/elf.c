3065d597edb330037644e98065ee6e26c7659186
elf <elf198012@gmail.com>
2015-04-20 11:00:18 +0800

    Rename 'FixedWorkerThread.h' to 'FixedTaskThread.h'

--- include/elf/FixedTaskThread.h
+++ include/elf/FixedTaskThread.h
@@ -1,5 +1,5 @@
-#ifndef FIXEDWORKERTHREAD_H
-#define FIXEDWORKERTHREAD_H
+#ifndef FIXEDTASKTHREAD_H
+#define FIXEDTASKTHREAD_H
 
 //License: Public Domain
 //Author: elf
@@ -13,17 +13,17 @@
 #define MAX_RUNNING_TASKS 64
 
 typedef struct structTask Task;
-typedef Bool (*TaskEntry)(Task *pTask);
+typedef Bool (*TaskHandler)(Task *pTask);
 typedef void (*TaskFinalize)(Task *pTask);
 
 struct structTask{
     DoubleNode node;
-    TaskEntry Entry;
+    TaskHandler Perform;
     TaskFinalize Finalize;
 };
 
-static inline void Task_Initialize(Task *pTask, TaskEntry Entry, TaskFinalize Finalize){
-    pTask->Entry = Entry;
+static inline void Task_Initialize(Task *pTask, TaskHandler Perform, TaskFinalize Finalize){
+    pTask->Perform = Perform;
     pTask->Finalize = Finalize;
 }
 
@@ -36,12 +36,12 @@ typedef struct{
     Task **ppRunningsEnd;
     Task **ppRunningsMax;
     List lstWaitings;
-}WorkerThread;
+}TaskThread;
 
-static void *FixedWorkerThread_Entry(WorkerThread *pThread);
+static void *FixedTaskThread_Entry(TaskThread *pThread);
 
-static inline void FixedWorkerThread_Initialize(WorkerThread *pThread){
-    Thread_Initialize((Thread*)pThread, (ThreadEntry)FixedWorkerThread_Entry);
+static inline void FixedTaskThread_Initialize(TaskThread *pThread){
+    Thread_Initialize((Thread*)pThread, (ThreadEntry)FixedTaskThread_Entry);
     ThreadLock_Initialize(&pThread->lock);
     ThreadCondition_Initialize(&pThread->condition);
     pThread->ppRunningsEnd = pThread->szRunnings;
@@ -49,16 +49,16 @@ static inline void FixedWorkerThread_Initialize(WorkerThread *pThread){
     List_Initialize(&pThread->lstWaitings);
 }
 
-static inline void FixedWorkerThread_Finalize(WorkerThread *pThread){
+static inline void FixedTaskThread_Finalize(TaskThread *pThread){
     ThreadCondition_Finalize(&pThread->condition);
     ThreadLock_Finalize(&pThread->lock);
 }
 
-static inline Bool FixedWorkerThread_Run(WorkerThread *pThread){
+static inline Bool FixedTaskThread_Run(TaskThread *pThread){
     return Thread_Run((Thread*)pThread);
 }
 
-static inline void FixedWorkerThread_Post(WorkerThread *pThread, Task *pTask){
+static inline void FixedTaskThread_Post(TaskThread *pThread, Task *pTask){
     ThreadLock_Lock(&pThread->lock);
     List_Push(&pThread->lstWaitings, (DoubleNode*)pTask);
     ThreadLock_Unlock(&pThread->lock);
@@ -67,7 +67,7 @@ static inline void FixedWorkerThread_Post(WorkerThread *pThread, Task *pTask){
 }
 
 //Do not post Empty task list
-static inline void FixedWorkerThread_PostTasks(WorkerThread *pThread, List *pTasks){
+static inline void FixedTaskThread_PostTasks(TaskThread *pThread, List *pTasks){
     ThreadLock_Lock(&pThread->lock);
     List_MoveTo(pTasks, &pThread->lstWaitings);
     ThreadLock_Unlock(&pThread->lock);
@@ -76,43 +76,43 @@ static inline void FixedWorkerThread_PostTasks(WorkerThread *pThread, List *pTas
 }
 
 //Internal
-static inline Bool FixedWorkerThread_HaveWaitings(List *pTasks){
+static inline Bool FixedTaskThread_HaveWaitings(List *pTasks){
     return List_NotEmpty(pTasks);
 }
 
-static inline Bool FixedWorkerThread_NoTasks(List *pTasks){
+static inline Bool FixedTaskThread_NoTasks(List *pTasks){
     return List_Empty(pTasks);
 }
 
-static inline void FixedWorkerThread_FillTasks(WorkerThread *pThread){
+static inline void FixedTaskThread_FillTasks(TaskThread *pThread){
     Task **ppRunningsMax = pThread->ppRunningsMax;
     List *pWaitings = &pThread->lstWaitings;
 
-    while(pThread->ppRunningsEnd != ppRunningsMax && FixedWorkerThread_HaveWaitings(pWaitings)){
+    while(pThread->ppRunningsEnd != ppRunningsMax && FixedTaskThread_HaveWaitings(pWaitings)){
         Task *pTask = (Task *)List_Pop(pWaitings);
         *pThread->ppRunningsEnd++ = pTask;
     }
 }
 
-static inline void FixedWorkerThread_WaitTasks(WorkerThread *pThread){
+static inline void FixedTaskThread_WaitTasks(TaskThread *pThread){
     ThreadLock_Lock(&pThread->lock);
 
-    FixedWorkerThread_FillTasks(pThread);
+    FixedTaskThread_FillTasks(pThread);
 
     if(pThread->ppRunningsEnd == pThread->szRunnings){
         ThreadCondition_Wait(&pThread->condition, &pThread->lock);
-        FixedWorkerThread_FillTasks(pThread);
+        FixedTaskThread_FillTasks(pThread);
     }
 
     ThreadLock_Unlock(&pThread->lock);
 }
 
-static inline void FixedWorkerThread_RunTasks(WorkerThread *pThread){
+static inline void FixedTaskThread_RunTasks(TaskThread *pThread){
     Task **ppTask = pThread->szRunnings;
     Task **ppEnd = pThread->ppRunningsEnd;
     while(ppTask != ppEnd){
         Task *pTask = *ppTask;
-        if(pTask->Entry(pTask)){
+        if(pTask->Perform(pTask)){
             pTask->Finalize(pTask);
             *ppTask = *--ppEnd;
             pThread->ppRunningsEnd= ppEnd;
@@ -122,12 +122,12 @@ static inline void FixedWorkerThread_RunTasks(WorkerThread *pThread){
     }
 }
 
-static void *FixedWorkerThread_Entry(WorkerThread *pThread){
+static void *FixedTaskThread_Entry(TaskThread *pThread){
     while(true){
-        FixedWorkerThread_WaitTasks(pThread);
-        FixedWorkerThread_RunTasks(pThread);
+        FixedTaskThread_WaitTasks(pThread);
+        FixedTaskThread_RunTasks(pThread);
     }
     return null;
 }
-#endif //FIXEDWORKERTHREAD_H
+#endif //FIXEDTASKTHREAD_H
 
