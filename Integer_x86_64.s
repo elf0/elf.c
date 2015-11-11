0499eeadc1a28e1b8ba8a73688345549d5844a1f
elf <elf198012@gmail.com>
2015-11-11 21:52:03 +0800

    Add sub function for 'Unsigned integer'

--- Integer_x86_64.s
+++ Integer_x86_64.s
@@ -1,5 +1,5 @@
 .text
-.globl UI_Add
+.globl UI_Add, UI_Sub
 UI_Add:
 movq (%rsi), %rax
 addq %rax, (%rdi)
@@ -12,3 +12,17 @@ decq %rdx
 jnz 0b
 setcb %al
 retq
+
+UI_Sub:
+movq (%rsi), %rax
+subq %rax, (%rdi)
+0:
+leaq 8(%rdi), %rdi
+leaq 8(%rsi), %rsi
+movq (%rsi), %rax
+sbbq %rax, (%rdi)
+decq %rdx
+jnz 0b
+setcb %al
+retq
+
