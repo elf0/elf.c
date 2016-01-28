46d3e38e5a47ad8b400a384e86c10e149c2ca003
elf <elf198012@gmail.com>
2016-01-28 18:38:11 +0800

    Add "static inline" to functions

--- include/elf/BinarySearch.h
+++ include/elf/BinarySearch.h
@@ -7,7 +7,7 @@
 
 //All uValues must not be 0!
 
-Bool U8Array_BinarySearch(U8 *pBegin, U8 *pEnd, U8 uValue, U8 **ppPosition){
+static inline Bool U8Array_BinarySearch(U8 *pBegin, U8 *pEnd, U8 uValue, U8 **ppPosition){
   U8 *pB = pBegin, *pE = pEnd, *pMid;
   U32 i;
   while(i = pE - pB >> 1){
@@ -26,7 +26,7 @@ Bool U8Array_BinarySearch(U8 *pBegin, U8 *pEnd, U8 uValue, U8 **ppPosition){
   return uValue == *pB;
 }
 
-Bool U16Array_BinarySearch(U16 *pBegin, U16 *pEnd, U16 uValue, U16 **ppPosition){
+static inline Bool U16Array_BinarySearch(U16 *pBegin, U16 *pEnd, U16 uValue, U16 **ppPosition){
   U16 *pB = pBegin, *pE = pEnd, *pMid;
   U32 i;
   while(i = pE - pB >> 1){
@@ -45,7 +45,7 @@ Bool U16Array_BinarySearch(U16 *pBegin, U16 *pEnd, U16 uValue, U16 **ppPosition)
   return uValue == *pB;
 }
 
-Bool U32Array_BinarySearch(U32 *pBegin, U32 *pEnd, U32 uValue, U32 **ppPosition){
+static inline Bool U32Array_BinarySearch(U32 *pBegin, U32 *pEnd, U32 uValue, U32 **ppPosition){
   U32 *pB = pBegin, *pE = pEnd, *pMid;
   U32 i;
   while(i = pE - pB >> 1){
@@ -64,7 +64,7 @@ Bool U32Array_BinarySearch(U32 *pBegin, U32 *pEnd, U32 uValue, U32 **ppPosition)
   return uValue == *pB;
 }
 
-Bool U64Array_BinarySearch(U64 *pBegin, U64 *pEnd, U64 uValue, U64 **ppPosition){
+static inline Bool U64Array_BinarySearch(U64 *pBegin, U64 *pEnd, U64 uValue, U64 **ppPosition){
   U64 *pB = pBegin, *pE = pEnd, *pMid;
   U32 i;
   while(i = pE - pB >> 1){
@@ -83,7 +83,7 @@ Bool U64Array_BinarySearch(U64 *pBegin, U64 *pEnd, U64 uValue, U64 **ppPosition)
   return uValue == *pB;
 }
 
-Bool I8Array_BinarySearch(I8 *pBegin, I8 *pEnd, I8 uValue, I8 **ppPosition){
+static inline Bool I8Array_BinarySearch(I8 *pBegin, I8 *pEnd, I8 uValue, I8 **ppPosition){
   I8 *pB = pBegin, *pE = pEnd, *pMid;
   U32 i;
   while(i = pE - pB >> 1){
@@ -102,7 +102,7 @@ Bool I8Array_BinarySearch(I8 *pBegin, I8 *pEnd, I8 uValue, I8 **ppPosition){
   return uValue == *pB;
 }
 
-Bool I16Array_BinarySearch(I16 *pBegin, I16 *pEnd, I16 iValue, I16 **ppPosition){
+static inline Bool I16Array_BinarySearch(I16 *pBegin, I16 *pEnd, I16 iValue, I16 **ppPosition){
   I16 *pB = pBegin, *pE = pEnd, *pMid;
   U32 i;
   while(i = pE - pB >> 1){
@@ -121,7 +121,7 @@ Bool I16Array_BinarySearch(I16 *pBegin, I16 *pEnd, I16 iValue, I16 **ppPosition)
   return iValue == *pB;
 }
 
-Bool I32Array_BinarySearch(I32 *pBegin, I32 *pEnd, I32 iValue, I32 **ppPosition){
+static inline Bool I32Array_BinarySearch(I32 *pBegin, I32 *pEnd, I32 iValue, I32 **ppPosition){
   I32 *pB = pBegin, *pE = pEnd, *pMid;
   U32 i;
   while(i = pE - pB >> 1){
@@ -140,7 +140,7 @@ Bool I32Array_BinarySearch(I32 *pBegin, I32 *pEnd, I32 iValue, I32 **ppPosition)
   return iValue == *pB;
 }
 
-Bool I64Array_BinarySearch(I64 *pBegin, I64 *pEnd, I64 iValue, I64 **ppPosition){
+static inline Bool I64Array_BinarySearch(I64 *pBegin, I64 *pEnd, I64 iValue, I64 **ppPosition){
   I64 *pB = pBegin, *pE = pEnd, *pMid;
   U32 i;
   while(i = pE - pB >> 1){
@@ -159,7 +159,7 @@ Bool I64Array_BinarySearch(I64 *pBegin, I64 *pEnd, I64 iValue, I64 **ppPosition)
   return iValue == *pB;
 }
 
-Bool F32Array_BinarySearch(F32 *pBegin, F32 *pEnd, F32 fValue, F32 **ppPosition){
+static inline Bool F32Array_BinarySearch(F32 *pBegin, F32 *pEnd, F32 fValue, F32 **ppPosition){
   F32 *pB = pBegin, *pE = pEnd, *pMid;
   U32 i;
   while(i = pE - pB >> 1){
@@ -178,7 +178,7 @@ Bool F32Array_BinarySearch(F32 *pBegin, F32 *pEnd, F32 fValue, F32 **ppPosition)
   return fValue == *pB;
 }
 
-Bool F64Array_BinarySearch(F64 *pBegin, F64 *pEnd, F64 fValue, F64 **ppPosition){
+static inline Bool F64Array_BinarySearch(F64 *pBegin, F64 *pEnd, F64 fValue, F64 **ppPosition){
   F64 *pB = pBegin, *pE = pEnd, *pMid;
   U32 i;
   while(i = pE - pB >> 1){
