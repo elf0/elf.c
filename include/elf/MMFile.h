d0bec497ba550741422bf147b9b92a5a92d5d4f3
elf <elf198012@gmail.com>
2016-06-28 16:34:58 +0800

    Change "MMFile_SetSize()" to "MMFile_Adjust()"

--- include/elf/MMFile.h
+++ include/elf/MMFile.h
@@ -7,64 +7,94 @@
 
 #include "File.h"
 #include <sys/mman.h>
+/*
+#define PAGE_SHIFT 12
+//4096
+#define PAGE_SIZE (1 << PAGE_SHIFT)
+#define PAGE_SIZE_MASK_L (PAGE_SIZE - 1)
+#define PAGE_SIZE_MASK (~PAGE_SIZE_MASK_L)
+#define PAGE_ALIGN(s) ((s + PAGE_SIZE_MASK_L) & PAGE_SIZE_MASK)
+#define PAGES_TO_SIZE(pages) (pages << PAGE_SHIFT)
+*/
 
 typedef struct{
   File file;
   Byte *pBegin;
 }MMFile;
 
-static inline Byte *MMFile_Open(MMFile *pFile, const C *szPath){
+static inline E8 MMFile_Adjust(MMFile *pFile, I32 iSize);
+
+static inline E8 MMFile_Open(MMFile *pFile, const C *szPath){
   File *pfFile = &pFile->file;
   if(!File_Open(pfFile, szPath))
-    return null;
+    return 1;
 
   if(!File_ReadMeta(pfFile)){
     File_Close(pfFile);
-    return null;
+    return 2;
   }
 
   Byte *pBegin = (Byte*)mmap(null, pfFile->meta.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, pfFile->fd, 0);
   if(pBegin == MAP_FAILED){
     File_Close(pfFile);
-    return pFile->pBegin = null;
+    pFile->pBegin = null;
+    return 3;
   }
-  return pFile->pBegin = pBegin;
+
+  pFile->pBegin = pBegin;
+  return 0;
 }
 
-static inline Byte *MMFile_Prepare(MMFile *pFile, const C *szPath){
+static inline E8 MMFile_Prepare(MMFile *pFile, const C *szPath, U32 uMinSize){
   File *pfFile = &pFile->file;
   if(!File_Prepare(pfFile, szPath))
-    return null;
+    return 1;
 
   if(!File_ReadMeta(pfFile)){
     File_Close(pfFile);
-    return null;
+    return 2;
+  }
+
+  U64 uSize = File_GetSize(pfFile);
+  if(!uSize){
+   if(!uMinSize)
+    uMinSize = 4096;
+
+   E8 e = MMFile_Adjust(pFile, uMinSize);
+   if(e){
+    File_Close(pfFile);
+    return e;
+   }
   }
 
   Byte *pBegin = (Byte*)mmap(null, pfFile->meta.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, pfFile->fd, 0);
   if(pBegin == MAP_FAILED){
     File_Close(pfFile);
-    return pFile->pBegin = null;
+    pFile->pBegin = null;
+    return 3;
   }
-  return pFile->pBegin = pBegin;
+  pFile->pBegin = pBegin;
+  return 0;
 }
 
-static inline const Byte *MMFile_OpenForRead(MMFile *pFile, const C *szPath){
+static inline const E8 MMFile_OpenForRead(MMFile *pFile, const C *szPath){
   File *pfFile = &pFile->file;
   if(!File_OpenForRead(pfFile, szPath))
-    return null;
+    return 1;
 
   if(!File_ReadMeta(pfFile)){
     File_Close(pfFile);
-    return null;
+    return 2;
   }
 
   Byte *pBegin = (Byte*)mmap(null, pfFile->meta.st_size, PROT_READ, MAP_SHARED, pfFile->fd, 0);
   if(pBegin == MAP_FAILED){
     File_Close(pfFile);
-    return pFile->pBegin = null;
+    pFile->pBegin = null;
+    return 3;
   }
-  return pFile->pBegin = pBegin;
+  pFile->pBegin = pBegin;
+  return 0;
 }
 
 static inline void MMFile_Close(MMFile *pFile){
@@ -83,15 +113,26 @@ static inline U64 MMFile_GetSize(MMFile *pFile){
   return File_GetSize(&pFile->file);
 }
 
-static inline Byte *MMFile_SetSize(MMFile *pFile, U64 nSize){
+static inline E8 MMFile_Adjust(MMFile *pFile, I32 iSize){
   File *pfFile = &pFile->file;
-  munmap(pFile->pBegin, pfFile->meta.st_size);
-  if(!File_SetSize(pfFile, nSize))
-    return pFile->pBegin = null;
-  pfFile->meta.st_size = nSize;
+  U64 uFileSize = MMFile_GetSize(pFile);
+  munmap(pFile->pBegin, uFileSize);
 
-  Byte *pBegin = (Byte*)mmap(null, nSize, PROT_READ | PROT_WRITE, MAP_SHARED, pfFile->fd, 0);
-  return  pFile->pBegin = pBegin != MAP_FAILED? pBegin: null;
+  uFileSize += iSize;
+  if(!File_SetSize(pfFile, uFileSize)){
+    pFile->pBegin = null;
+    return 1;
+  }
+  pfFile->meta.st_size = uFileSize;
+
+  Byte *pBegin = (Byte*)mmap(null, uFileSize, PROT_READ | PROT_WRITE, MAP_SHARED, pfFile->fd, 0);
+  if(pBegin == MAP_FAILED){
+    pFile->pBegin = null;
+    return 2;
+  }
+
+  pFile->pBegin = pBegin;
+  return 0;
 }
 
 static inline B MMFile_Flush(MMFile *pFile){
