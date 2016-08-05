e1c05eb82a618859b58bde8eb9ff7d27fcb327d1
elf <elf198012@gmail.com>
2016-08-05 21:56:13 +0800

    Modify "KVReader.h"

--- include/elf/KVReader.h
+++ include/elf/KVReader.h
@@ -12,8 +12,22 @@
 
 typedef E8 (*KVReader_Handler)(void *pContext, const C *pBegin, const C *pEnd);
 
-static inline E8 KVReader_Parse(void *pContext, const C *pBegin, const C *pEnd
-, KVReader_Handler onKey, KVReader_Handler onValue){
+typedef struct{
+  void *pContext;
+  KVReader_Handler onKey;
+  KVReader_Handler onValue;
+}KVReader;
+
+static inline void KVReader_Initialize(KVReader *pReader
+                                       , void *pContext
+                                       , KVReader_Handler onKey
+                                       , KVReader_Handler onValue){
+  pReader->pContext = pContext;
+  pReader->onKey = onKey;
+  pReader->onValue = onValue;
+}
+
+static inline E8 KVReader_Parse(KVReader *pReader, const C *pBegin, const C *pEnd){
   size_t nSize = pEnd - pBegin;
   if(nSize < 2)
     return 1;
@@ -26,24 +40,23 @@ static inline E8 KVReader_Parse(void *pContext, const C *pBegin, const C *pEnd
   const C *p = pBegin;
   while(1){
     p = String_SkipUntil(pData = p, (C)':');
-    if(err = onKey(pContext, pData, p++))
+    if(err = pReader->onKey(pReader->pContext, pData, p++))
       return err;
 
     while(1){
-     p = String_SkipUntil(pData = p, (C)'\n');
-     if(err = onValue(pContext, pData, p++))
-      return err;
+      p = String_SkipUntil(pData = p, (C)'\n');
+      if(err = pReader->onValue(pReader->pContext, pData, p++))
+        return err;
 
-     if(p == pEnd)
-      return 0;
+      if(p == pEnd)
+        return 0;
 
-     if(*p != (C)':')
-      break;
+      if(*p != (C)':')
+        break;
 
-     ++p;
+      ++p;
     }
   }
 }
 
 #endif // KVREADER_H
-
