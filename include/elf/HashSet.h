fa1bcf7824e388e50ae6b23d688f22d2624443ce
elf <elf198012@gmail.com>
2016-08-15 18:24:10 +0800

    Add "HashSet.h"

---
+++ include/elf/HashSet.h
@@ -0,0 +1,66 @@
+#ifndef HASHSET_H
+#define HASHSET_H
+
+//License: Public Domain
+//Author: elf
+//EMail: elf@elf0.org
+
+#include "RBTree.h"
+
+//API
+typedef struct HashSet  HashSet;
+typedef struct HashSet_Node HashSet_Node;
+
+typedef I8 (*HashSet_FindCompare_f)(Byte *pKey, U32 uKey, HashSet_Node *pNode);
+typedef I8 (*HashSet_InsertCompare_f)(HashSet_Node *pNew, HashSet_Node *pNode);
+
+static inline void HashSet_Initialize(HashSet *pSet);
+static inline HashSet_Node *HashSet_Find(HashSet *pSet, Byte *pKey, U32 uKey, HashSet_FindCompare_f fCompare);
+static inline B HashSet_Insert(HashSet *pSet, HashSet_Node *pNew, HashSet_InsertCompare_f fCompare);
+
+//Internal
+struct HashSet_Node{
+  RBTree_Node node;
+  U32 uData;
+  Byte szData[4];
+};
+
+#define SET_HASH_WIDTH 16
+struct HashSet{
+  RBTree trees[SET_HASH_WIDTH];
+  U32 uCount;
+};
+
+static inline void HashSet_Initialize(HashSet *pSet){
+  RBTree *pTree = pSet->trees;
+  RBTree *ptEnd = pTree + SET_HASH_WIDTH;
+  while(pTree != ptEnd)
+    RBTree_Initialize(pTree++);
+
+  pSet->uCount = 0;
+}
+
+static inline HashSet_Node *HashSet_Find(HashSet *pSet, Byte *pKey, U32 uKey, HashSet_FindCompare_f fCompare){
+  return (HashSet_Node*)RBTree_Find(&pSet->trees[uKey & 0xF], pKey, uKey, (RBTree_FindCompare_f)fCompare);
+}
+
+static inline E8 HashSet_Insert(HashSet *pSet, HashSet_Node *pNew, HashSet_InsertCompare_f fCompare){
+  RBTree *pTree = &pSet->trees[pNew->uData & 0xF];
+  E8 e = pTree->fAdd(pTree, (RBTree_Node*)pNew, (RBTree_AddCompare_f)fCompare);
+  if(e)
+    return e;
+
+  ++pSet->uCount;
+  return 0;
+}
+
+//RBTree_Remove not implement yet
+static inline void HashSet_Remove(HashSet *pSet, HashSet_Node *pNode){
+  RBTree *pTree = &pSet->trees[pNode->uData & 0xF];
+  RBTree_Remove(pTree, (RBTree_Node*)pNode);
+  --pSet->uCount;
+}
+
+
+#endif // HASHSET_H
+
--- include/elf/RBTree.h
+++ include/elf/RBTree.h
@@ -9,11 +9,12 @@
 typedef struct RBTree  RBTree;
 typedef struct RBTree_Node RBTree_Node;
 
-typedef I8 (*RBTree_Compare_Function)(RBTree_Node *pLeft, RBTree_Node *pRight);
+typedef I8 (*RBTree_FindCompare_f)(Byte *pKey, U32 uKey, RBTree_Node *pNode);
+typedef I8 (*RBTree_AddCompare_f)(RBTree_Node *pNew, RBTree_Node *pNode);
 
-static inline void RBTree_Initialize(RBTree *pTree, RBTree_Compare_Function fCompare);
-static inline RBTree_Node *RBTree_Find(RBTree *pTree, RBTree_Node *pKey);
-static inline B RBTree_Insert(RBTree *pTree, RBTree_Node *pNew);
+static inline void RBTree_Initialize(RBTree *pTree);
+static inline RBTree_Node *RBTree_Find(RBTree *pTree, Byte *pKey, U32 uKey, RBTree_FindCompare_f fCompare);
+static inline B RBTree_Add(RBTree *pTree, RBTree_Node *pNew, RBTree_AddCompare_f fCompare);
 
 //Internal
 struct RBTree_Node{
@@ -29,31 +30,27 @@ struct RBTree_Node{
 #define RBTREE_NODE_PARENT(pNode) (*(RBTree_Node**)pNode)
 #define RBTREE_NODE_SET_PARENT(pNode, pParent) (RBTREE_NODE_PARENT(pNode) = pParent)
 
-typedef E8 (*RBTree_Insert_Function) (RBTree *pTree, RBTree_Node *pNode);
+typedef E8 (*RBTree_Add_F) (RBTree *pTree, RBTree_Node *pNode, RBTree_AddCompare_f fCompare);
 
-static inline E8 RBTree_InsertRoot(RBTree *pTree, RBTree_Node *pNew);
-static inline E8 RBTree_InsertChild(RBTree *pTree, RBTree_Node *pNew);
+static inline E8 RBTree_AddRoot(RBTree *pTree, RBTree_Node *pNew, RBTree_AddCompare_f fCompare);
+static inline E8 RBTree_AddChild(RBTree *pTree, RBTree_Node *pNew, RBTree_AddCompare_f fCompare);
 static inline void RBTree_Balance(RBTree *pTree, RBTree_Node *pNode);
 
 struct RBTree{
   RBTree_Node     *pRoot;
-  RBTree_Insert_Function   fInsert;
-  RBTree_Compare_Function fCompare;
-  U32 uCount;
+  RBTree_Add_F   fAdd;
 };
 
-static inline void RBTree_Initialize(RBTree *pTree, RBTree_Compare_Function fCompare){
+static inline void RBTree_Initialize(RBTree *pTree){
   pTree->pRoot = 0;
-  pTree->fInsert = RBTree_InsertRoot;
-  pTree->fCompare = fCompare;
-  pTree->uCount = 0;
+  pTree->fAdd = RBTree_AddRoot;
 }
 
-static inline RBTree_Node *RBTree_Find(RBTree *pTree, RBTree_Node *pKey){
+static inline RBTree_Node *RBTree_Find(RBTree *pTree, Byte *pKey, U32 uKey, RBTree_FindCompare_f fCompare){
   RBTree_Node *pNode = RBTREE_ROOT(pTree);
   I8 iCompare;
   while(pNode){
-    iCompare = pTree->fCompare(pKey, pNode);
+    iCompare = fCompare(pKey, uKey, pNode);
     if(iCompare < 0)
       pNode = pNode->pLeft;
     else if(iCompare > 0)
@@ -65,28 +62,26 @@ static inline RBTree_Node *RBTree_Find(RBTree *pTree, RBTree_Node *pKey){
   return 0;
 }
 
-static inline E8 RBTree_Insert(RBTree *pTree, RBTree_Node *pNew){
-  return pTree->fInsert(pTree, pNew);
+static inline E8 RBTree_Add(RBTree *pTree, RBTree_Node *pNew, RBTree_AddCompare_f fCompare){
+  return pTree->fAdd(pTree, pNew, fCompare);
 }
 
-static inline E8 RBTree_InsertRoot(RBTree *pTree, RBTree_Node *pNew){
+static inline E8 RBTree_AddRoot(RBTree *pTree, RBTree_Node *pNew, RBTree_AddCompare_f fCompare){
   pNew->pLeft = 0;
   pNew->pRight = 0;
   RBTREE_NODE_SET_PARENT(pNew, 0);
   pNew->bRed = 0;
 
   RBTREE_SET_ROOT(pTree, pNew);
-  pTree->fInsert = RBTree_InsertChild;
-  pTree->uCount = 1;
+  pTree->fAdd = RBTree_AddChild;
   return 0;
 }
 
-static inline E8 RBTree_InsertChild(RBTree *pTree, RBTree_Node *pNew){
+static inline E8 RBTree_AddChild(RBTree *pTree, RBTree_Node *pNew, RBTree_AddCompare_f fCompare){
   RBTree_Node *pNode = RBTREE_ROOT(pTree);
-  RBTree_Compare_Function Compare = pTree->fCompare;
   I8 iCompare;
   while(1){
-    iCompare = Compare(pNew, pNode);
+    iCompare = fCompare(pNew, pNode);
     if(iCompare < 0){
       if(pNode->pLeft)
         pNode = pNode->pLeft;
@@ -113,12 +108,12 @@ static inline E8 RBTree_InsertChild(RBTree *pTree, RBTree_Node *pNew){
   pNew->bRed = 1;
 
   RBTree_Balance(pTree, pNew);
-  ++pTree->uCount;
   return 0;
 }
 
 //Not implement yet
-void RBTree_Delete(RBTree *pTree, RBTree_Node *pNode);
+static inline void RBTree_Remove(RBTree *pTree, RBTree_Node *pNode){
+}
 
 static inline void RBTree_Balance(RBTree *pTree, RBTree_Node *pNode){
   RBTree_Node *pGreatGrandparent;
