3f3326a36325f2e2d34b275772b06d4afc8ff363
elf0 <elf@elf0.org>
2017-10-25 18:08:44 +0800

    fix DoubleNode's bug

--- include/elf/DoubleNode.h
+++ include/elf/DoubleNode.h
@@ -13,56 +13,76 @@ struct struct_DoubleNode{
     DoubleNode *pPrev;
 };
 
-static inline void DoubleNode_LinkPrev(DoubleNode *pNode, DoubleNode *pPrev){
+inline
+static void DoubleNode_LinkPrev(DoubleNode *pNode, DoubleNode *pPrev){
     pNode->pPrev = pPrev;
     pPrev->pNext = pNode;
 }
 
-static inline void DoubleNode_LinkNext(DoubleNode *pNode, DoubleNode *pNext){
+inline
+static void DoubleNode_LinkNext(DoubleNode *pNode, DoubleNode *pNext){
     pNode->pNext = pNext;
     pNext->pPrev = pNode;
 }
 
-static inline void DoubleNode_Link(DoubleNode *pNode, DoubleNode *pPrev, DoubleNode *pNext){
+inline
+static void DoubleNode_Link(DoubleNode *pNode, DoubleNode *pPrev, DoubleNode *pNext){
     DoubleNode_LinkPrev(pNode, pPrev);
     DoubleNode_LinkNext(pNode, pNext);
 }
 
-static inline void DoubleNode_Unlink(DoubleNode *pNode){
+inline
+static void DoubleNode_Unlink(DoubleNode *pNode){
     DoubleNode *pNext = pNode->pNext;
     DoubleNode *pPrev = pNode->pPrev;
     pPrev->pNext = pNext;
     pNext->pPrev = pPrev;
 }
 
-static inline void DoubleNode_Reset(DoubleNode *pNode){
+inline
+static void DoubleNode_Reset(DoubleNode *pNode){
     pNode->pPrev = pNode->pNext = pNode;
 }
 
-static inline B DoubleNode_Alone(const DoubleNode *pNode){
+inline
+static B DoubleNode_Alone(const DoubleNode *pNode){
     return pNode->pNext == pNode;
 }
 
-static inline B DoubleNode_NotAlone(const DoubleNode *pNode){
+inline
+static B DoubleNode_NotAlone(const DoubleNode *pNode){
     return pNode->pNext != pNode;
 }
 
-static inline void DoubleNode_MoveBuddiesTo(DoubleNode *pNode, DoubleNode *pTargetNode){
+inline
+static void DoubleNode_MoveBuddiesTo(DoubleNode *pNode, DoubleNode *pTargetNode){
     DoubleNode_LinkPrev(pNode->pNext, pTargetNode->pPrev);
     DoubleNode_LinkNext(pNode->pPrev, pTargetNode);
     DoubleNode_Reset(pNode);
 }
 
-static inline void DoubleNode_MoveBuddiesToFront(DoubleNode *pNode, DoubleNode *pTargetNode){
+inline
+static void DoubleNode_MoveBuddiesToFront(DoubleNode *pNode, DoubleNode *pTargetNode){
     DoubleNode_LinkNext(pNode->pPrev, pTargetNode->pNext);
     DoubleNode_LinkPrev(pNode->pNext, pTargetNode);
     DoubleNode_Reset(pNode);
 }
 
-static inline void DoubleNode_SwapBuddies(DoubleNode *pNode, DoubleNode *pTargetNode){
-    DoubleNode tmp = *pNode;
-    *pNode = *pTargetNode;
-    *pTargetNode = tmp;
+inline
+static void DoubleNode_Swap(DoubleNode *pNode, DoubleNode *pTargetNode){
+    pNode->pNext->pPrev = pTargetNode
+    pNode->pPrev->pNext = pTargetNode
+
+    pTargetNode->pNext->pPrev = pNode
+    pTargetNode->pPrev->pNext = pNode
+
+    DoubleNode *pTmp = pNode->pNext
+    pNode->pNext = pTargetNode->pNext
+    pTargetNode->pNext = pTmp
+
+    pTmp = pNode->pPrev
+    pNode->pPrev = pTargetNode->pPrev
+    pTargetNode->pPrev = tmp
 }
 
 #endif //DOUBLENODE_H
--- include/elf/List.h
+++ include/elf/List.h
@@ -11,105 +11,128 @@ typedef struct{
     DoubleNode entry;
 }List;
 
-static inline void List_Clear(List *pList){
+inline
+static void List_Clear(List *pList){
     DoubleNode_Reset((DoubleNode*)pList);
 }
 
-static inline void List_Initialize(List *pList){
+inline
+static void List_Initialize(List *pList){
     List_Clear(pList);
 }
 
-static inline B List_Empty(const List *pList){
+inline
+static B List_Empty(const List *pList){
     return DoubleNode_Alone((DoubleNode*)pList);
 }
 
-static inline B List_NotEmpty(const List *pList){
+inline
+static B List_NotEmpty(const List *pList){
     return DoubleNode_NotAlone((DoubleNode*)pList);
 }
 
-static inline DoubleNode *List_Front(const List *pList){
+inline
+static DoubleNode *List_Front(const List *pList){
     return pList->entry.pNext;
 }
 
-static inline DoubleNode *List_Back(const List *pList){
+inline
+static DoubleNode *List_Back(const List *pList){
     return pList->entry.pPrev;
 }
 
-static inline B List_First(const List *pList, const DoubleNode *pNode){
+inline
+static B List_First(const List *pList, const DoubleNode *pNode){
     return pNode->pPrev == ((DoubleNode*)pList);
 }
 
-static inline B List_NotFirst(const List *pList, const DoubleNode *pNode){
+inline
+static B List_NotFirst(const List *pList, const DoubleNode *pNode){
     return pNode->pPrev != ((DoubleNode*)pList);
 }
 
-static inline B List_Last(const List *pList, const DoubleNode *pNode){
+inline
+static B List_Last(const List *pList, const DoubleNode *pNode){
     return pNode->pNext == ((DoubleNode*)pList);
 }
 
-static inline B List_NotLast(const List *pList, const DoubleNode *pNode){
+inline
+static B List_NotLast(const List *pList, const DoubleNode *pNode){
     return pNode->pNext != ((DoubleNode*)pList);
 }
 
-static inline void List_Insert(DoubleNode *pNode, DoubleNode *pPrev, DoubleNode *pNext){
+inline
+static void List_Insert(DoubleNode *pNode, DoubleNode *pPrev, DoubleNode *pNext){
     DoubleNode_Link(pNode, pPrev, pNext);
 }
 
-static inline void List_Remove(DoubleNode *pNode){
+inline
+static void List_Remove(DoubleNode *pNode){
     DoubleNode_Unlink(pNode);
 }
 
 //push back
-static inline void List_Push(List *pList, DoubleNode *pNode){
+inline
+static void List_Push(List *pList, DoubleNode *pNode){
     DoubleNode_Link(pNode, ((DoubleNode*)pList)->pPrev, (DoubleNode*)pList);
 }
 
 //pop front
-static inline DoubleNode *List_Pop(List *pList){
+inline
+static DoubleNode *List_Pop(List *pList){
     DoubleNode *pNode = ((DoubleNode*)pList)->pNext;
     DoubleNode_Unlink(pNode);
     return pNode;
 }
 
-static inline void List_PopTo(List *pList, List *pTarget){
+inline
+static void List_PopTo(List *pList, List *pTarget){
     List_Push(pTarget, List_Pop(pList));
 }
 
-static inline void List_PushFront(List *pList, DoubleNode *pNode){
+inline
+static void List_PushFront(List *pList, DoubleNode *pNode){
     DoubleNode_Link(pNode, (DoubleNode*)pList, ((DoubleNode*)pList)->pNext);
 }
 
-static inline void List_PopToFront(List *pList, List *pTarget){
+inline
+static void List_PopToFront(List *pList, List *pTarget){
     List_PushFront(pTarget, List_Pop(pList));
 }
 
-static inline DoubleNode *List_PopBack (List *pList){
+inline
+static DoubleNode *List_PopBack (List *pList){
     DoubleNode *pNode = ((DoubleNode*)pList)->pPrev;
     DoubleNode_Unlink(pNode);
     return pNode;
 }
 
-static inline void List_MoveNodeToBack(List *pList, DoubleNode *pNode){
+inline
+static void List_MoveNodeToBack(List *pList, DoubleNode *pNode){
     List_Remove(pNode);
     List_Push(pList, pNode);
 }
 
-static inline void List_MoveNodeToFront(List *pList, DoubleNode *pNode){
+inline
+static void List_MoveNodeToFront(List *pList, DoubleNode *pNode){
     List_Remove(pNode);
     List_PushFront(pList, pNode);
 }
 
 //move to back
-static inline void List_MoveTo(List *pList, List *pTargetList){
+inline
+static void List_MoveTo(List *pList, List *pTargetList){
     DoubleNode_MoveBuddiesTo((DoubleNode*)pList, (DoubleNode*)pTargetList);
 }
 
-static inline void List_MoveToFront(List *pList, List *pTargetList){
+inline
+static void List_MoveToFront(List *pList, List *pTargetList){
     DoubleNode_MoveBuddiesToFront((DoubleNode*)pList, (DoubleNode*)pTargetList);
 }
 
-static inline void List_Swap(List *pList, List *pTargetList){
-    DoubleNode_SwapBuddies((DoubleNode*)pList, (DoubleNode*)pTargetList);
+inline
+static void List_Swap(List *pList, List *pTargetList){
+    DoubleNode_Swap((DoubleNode*)pList, (DoubleNode*)pTargetList);
 }
 
 #endif //LIST_H
