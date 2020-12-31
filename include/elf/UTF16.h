feceeae4a9bcedf1ce074ce15c46966a1e2d86de
elf0 <elf000@zoho.com>
2020-12-31 11:35:52 +0800

    Modify UTF8.h and UTF16.h

--- include/elf/UTF16.h
+++ include/elf/UTF16.h
@@ -5,72 +5,142 @@
 //Author: elf
 //EMail: elf@iamelf.com
 
+// Unicode: [0, 0x10FFFF]
+// Surrogate: [0xD800, 0xDFFF]
+// High surrogate: [0xD800, 0xDBFF]
+// Low surrogate: [0xDC00, 0xDFFF]
 
-//static inline B UTF16_IsSurrogate(C16 c){
-// return (c >> 11) == 0x1B;
-//}
-
-static inline B UTF16_IsBasic(C16 c){
- return (c >> 11) != 0x1B;
+inline
+static B UTF16_IsBasic(C16 c) {
+    return (c >> 11) != 0x1B;
 }
 
-static inline B UTF16_IsLead(C16 c){
- return (c >> 10) == 0x36;
+inline
+static B UTF16_IsSurrogate(C16 c) {
+    return (c >> 11) == 0x1B;
 }
 
-static inline B UTF16_IsTrail(C16 c){
- return (c >> 10) == 0x37;
+inline
+static B UTF16_IsLead(C16 c) {
+    return (c >> 10) == 0x36;
 }
 
-static inline C32 UTF16Extra_GetValue(C16 lead, C16 trail){
- C32 nHigh = (C32)lead & 0x03FF;
- C32 nLow = (C32)trail & 0x03FF;
- return 0x10000 + ((nHigh << 10) | nLow);
+inline
+static B UTF16_NotLead(C16 c) {
+    return (c >> 10) != 0x36;
 }
 
-static inline C16 *UTF16_GetValidValue(C16 *pBegn, C16 *pEnd, C32 *pValue){
- C16 *p16 = pBegn;
- C16 v16 = *p16++;
-  if(UTF16_IsBasic(v16))
-   *pValue = v16;
-  else if(UTF16_IsLead(v16)){
-   C16 v16Trail = *p16;
-   if(p16 == pEnd || !UTF16_IsTrail(v16Trail))
-    return null;
-   *pValue = UTF16Extra_GetValue(v16, v16Trail);
-   ++p16;
-  }
-  else
-   return null;
- return p16;
+inline
+static B UTF16_IsTrail(C16 c) {
+    return (c >> 10) == 0x37;
+}
+
+inline
+static B UTF16_NotTrail(C16 c) {
+    return (c >> 10) != 0x37;
+}
+
+inline
+static C32 UTF16_Surrogate(C32 lead, C16 trail) {
+    lead &= 0x03FF;
+    lead <<= 10;
+    trail &= 0x03FF;
+    return 0x10000 + (lead | trail);
+}
+
+inline
+static U64 UTF8_Count(const C16 *p, const C16 *pEnd) {
+    U64 uCount = 0;
+    while (p != pEnd) {
+        ++uCount;
+        if ((*p++ >> 11) == 0x1B)
+            ++p;
+    }
+    return uCount;
+}
+
+inline
+static B UTF16_Valid(const C16 *p, const C16 *pEnd) {
+    while(p != pEnd) {
+        C16 c16 = *p++;
+        if ((c16 >> 11) == 0x1B) {
+            if (c16 > 0xDBFF || p == pEnd || (*p++ >> 10) != 0x37)
+                return 0;
+        }
+    }
+    return 1;
+}
+
+inline
+static C32 UTF16_Parse(const C16 **ppBegin, const C16 *pEnd) {
+    C16 *p = *ppBegin;
+    C32 c32 = *p++;
+    if ((c32 >> 11) == 0x1B) {
+        if (c > 0xDBFF || p == pEnd)
+            c32 |= 0x80000000;
+        else {
+            C16 c16 = *p++;
+            if ((c16 >> 10) == 0x37)
+                c32 = UTF16_Surrogate(c32, c16);
+            else {
+                --p;
+                c32 |= 0x80000000;
+            }
+        }
+    }
+    *ppBegin = p;
+    return c32;
+}
+inline
+static U64 UTF8_ParseCount(const C **ppBegin, const C *pEnd) {
+    U64 uCount = 0;
+    C16 *p = *ppBegin;
+    while (p != pEnd) {
+        C32 c32 = *p++;
+        if ((c32 >> 11) == 0x1B) {
+            if (c > 0xDBFF || p == pEnd) {
+                --p;
+                uCount |= 0x8000000000000000;
+                break;
+            }
+            else if ((*p++ >> 10) != 0x37) {
+                p -= 2;
+                uCount |= 0x8000000000000000;
+                break;
+            }
+        }
+        ++uCount;
+    }
+    *ppBegin = p;
+    return uCount;
 }
 
 //caller must input valid utf-16 string
-static inline C16 *UTF16_GetValue(C16 *pString, C32 *pValue){
- C16 *p16 = pString;
- C16 v16 = *p16++;
- if(UTF16_IsBasic(v16))
-  *pValue = v16;
- else
-  *pValue = UTF16Extra_GetValue(v16, *p16++);
- return p16;
+inline
+static C32 UTF16_Read(const C16 **ppString) {
+    const C16 *p = *ppString;
+    C32 c32 = *p++;
+    if ((c32 >> 11) == 0x1B)
+        c32 = UTF16_Surrogate(c32, *p++);
+    *ppString = p;
+    return c32;
 }
 
 //caller must input valid utf value
-static inline C16 *UTF_To16(C32 value, C16 *pBuffer){
- C16 *p = pBuffer;
- if(value < 0x10000){
-  assert(value < 0xD800 || value >= 0xE000);
-  *p = value;
- }
- else{
-  assert(value < 0x110000);
-  value -= 0x10000;
-  *p++ = 0xD800 | (value >> 10);
-  *p = 0xDC00 | (value & 0x03FF);
- }
+inline
+static C16 *UTF16_Write(C16 *pBuffer, C32 value) {
+    if (value < 0x10000) {
+        //        assert(value < 0xD800 || value > 0xDFFF);
+        *pBuffer = value;
+    }
+    else{
+        //        assert(value < 0x110000);
+        value -= 0x10000;
+        *pBuffer++ = 0xD800 | (value >> 10);
+        *pBuffer = 0xDC00 | (value & 0x03FF);
+    }
 
- return ++p;
+    return ++pBuffer;
 }
 
 #endif //UTF16_H
--- include/elf/UTF8.h
+++ include/elf/UTF8.h
@@ -5,8 +5,9 @@
 //Author: elf
 //EMail: elf@iamelf.com
 
-// Max unicode: 0x10FFFF
 // Max utf8: F4 8F BF BF
+// Unicode: [0, 0x10FFFF]
+// Surrogate: [0xD800, 0xDFFF]
 
 #define UNICODE_INVALID(c32) (c32 > 0x10FFFF)
 
@@ -37,6 +38,105 @@ static const C *UTF8_SkipTail(C cHead, const C *pTail) {
 }
 
 inline
+static U64 UTF8_Count(const C *p, const C *pEnd) {
+    U64 uCount = 0;
+    while (p != pEnd) {
+        ++uCount;
+        C c = *p++;
+        if (c > 0x7F) { //0xxxxxxx
+            ++p;
+            if (c > 0xDF) { //1110xxxx 10xxxxxx 10xxxxxx
+                ++p;
+                if (c > 0xEF) //11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+                    ++p;
+            }
+        }
+    }
+    return uCount;
+}
+
+//caller must input valid utf-8 string
+inline
+static C32 UTF8_ReadTail(C32 cHead, const C **ppTail) {
+    const C *p = *ppTail;
+    if (cHead < 0xE0) { // 110xxxxx 10xxxxxx
+        cHead &= 0x1F;
+        cHead <<= 6;
+    }
+    else {
+        if (cHead < 0xF0) {//1110xxxx 10xxxxxx 10xxxxxx
+            cHead &= 0x0F;
+            cHead <<= 12;
+        }
+        else {//11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+            cHead &= 0x07;
+            cHead <<= 18;
+            cHead |= (*p++ & 0x3F) << 12;
+        }
+        cHead |= (*p++ & 0x3F) << 6;
+    }
+    cHead |= *p++ & 0x3F;
+    *ppTail = p;
+    return cHead;
+}
+
+//caller must input valid utf-8 string
+inline
+static C32 UTF8_Read(const C **ppString) {
+    const C *p = *ppString;
+    C32 c32 = *p++;
+    if (c32 > 0x7F) { // 0xxxxxxx
+        if (c32 < 0xE0) { // 110xxxxx 10xxxxxx
+            c32 &= 0x1F;
+            c32 <<= 6;
+        }
+        else {
+            if (c32 < 0xF0) {//1110xxxx 10xxxxxx 10xxxxxx
+                c32 &= 0x0F;
+                c32 <<= 12;
+            }
+            else {//11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+                c32 &= 0x07;
+                c32 <<= 18;
+                c32 |= (*p++ & 0x3F) << 12;
+            }
+            c32 |= (*p++ & 0x3F) << 6;
+        }
+        c32 |= *p++ & 0x3F;
+    }
+    *ppString = p;
+    return c32;
+}
+
+//pBuffer size should >= 4 bytes
+//caller must input valid utf c32
+inline
+static C *UTF8_Write(C *pBuffer, C32 value) {
+    C *p = pBuffer;
+    if (value < 0x80) { // 0xxxxxxx
+        *p++ = value;
+        return p;
+    }
+
+    if (value < 0x0800) // 110xxxxx 10xxxxxx
+        *p++ = 0xC0 | (value >> 6);
+    else {
+        if (value < 0x10000) {//1110xxxx 10xxxxxx 10xxxxxx
+            //            assert(value < 0xD800 || value > 0xDFFF);
+            *p++ = 0xE0 | (value >> 12);
+        }
+        else {//11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+            //            assert(value < 0x110000);
+            *p++ = 0xF0 | (value >> 18);
+            *p++ = 0x80 | ((value >> 12) & 0x3F);
+        }
+        *p++ = 0x80 | ((value >> 6) & 0x3F);
+    }
+    *p++ = 0x80 | (value & 0x3F);
+    return p;
+}
+
+inline
 static B UTF8_Valid(const C *p, const C *pEnd) {
     while (p != pEnd) {
         C32 c32 = *p++;
@@ -56,7 +156,8 @@ static B UTF8_Valid(const C *p, const C *pEnd) {
                     return 0;
                 else {
                     C t0 = *p++;
-                    if (t0 < 0x80 || t0 > 0xBF || (t0 < 0xA0 && c32 == 0xE0))
+                    if (t0 < 0x80 || t0 > 0xBF || (c32 == 0xE0 && t0 < 0xA0)
+                            || (c32 == 0xED && t0 > 0x9F && t0 < 0xC0))
                         return 0;
                     else {
                         C t1 = *p++;
@@ -92,24 +193,6 @@ static B UTF8_Valid(const C *p, const C *pEnd) {
 }
 
 inline
-static U64 UTF8_Count(const C *p, const C *pEnd) {
-    U64 uCount = 0;
-    while (p != pEnd) {
-        ++uCount;
-        C c = *p++;
-        if (c > 0x7F) { //0xxxxxxx
-            ++p;
-            if (c > 0xDF) { //1110xxxx 10xxxxxx 10xxxxxx
-                ++p;
-                if (c > 0xEF) //11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
-                    ++p;
-            }
-        }
-    }
-    return uCount;
-}
-
-inline
 static C32 UTF8_Parse(const C **ppBegin, const C *pEnd) {
     const C *p = *ppBegin;
     C32 c32 = *p++;
@@ -137,7 +220,8 @@ static C32 UTF8_Parse(const C **ppBegin, const C *pEnd) {
                 c32 |= 0x80000000;
             else {
                 C t0 = *p++;
-                if (t0 < 0x80 || t0 > 0xBF || (t0 < 0xA0 && c32 == 0xE0)) {
+                if (t0 < 0x80 || t0 > 0xBF || (c32 == 0xE0 && t0 < 0xA0)
+                         || (c32 == 0xED && t0 > 0x9F && t0 < 0xC0)) {
                     --p;
                     c32 |= 0x80000000;
                 }
@@ -233,7 +317,8 @@ static U64 UTF8_ParseCount(const C **ppBegin, const C *pEnd) {
                 }
                 else {
                     C t0 = *p++;
-                    if (t0 < 0x80 || t0 > 0xBF || (t0 < 0xA0 && c32 == 0xE0)) {
+                    if (t0 < 0x80 || t0 > 0xBF || (c32 == 0xE0 && t0 < 0xA0)
+                             || (c32 == 0xED && t0 > 0x9F && t0 < 0xC0)) {
                         p -= 2;
                         uCount |= 0x8000000000000000;
                         break;
@@ -289,87 +374,6 @@ static U64 UTF8_ParseCount(const C **ppBegin, const C *pEnd) {
     }
     *ppBegin = p;
     return uCount;
-}
-
-//caller must input valid utf-8 string
-inline
-static C32 UTF8_ReadTail(C32 cHead, const C **ppTail) {
-    const C *p = *ppTail;
-    if (cHead < 0xE0) { // 110xxxxx 10xxxxxx
-        cHead &= 0x1F;
-        cHead <<= 6;
-    }
-    else {
-        if (cHead < 0xF0) {//1110xxxx 10xxxxxx 10xxxxxx
-            cHead &= 0x0F;
-            cHead <<= 12;
-        }
-        else {//11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
-            cHead &= 0x07;
-            cHead <<= 18;
-            cHead |= (*p++ & 0x3F) << 12;
-        }
-        cHead |= (*p++ & 0x3F) << 6;
-    }
-    cHead |= *p++ & 0x3F;
-    *ppTail = p;
-    return cHead;
-}
-
-//caller must input valid utf-8 string
-inline
-static C32 UTF8_Read(const C **ppString) {
-    const C *p = *ppString;
-    C32 c32 = *p++;
-    if (c32 > 0x7F) { // 0xxxxxxx
-        if (c32 < 0xE0) { // 110xxxxx 10xxxxxx
-            c32 &= 0x1F;
-            c32 <<= 6;
-        }
-        else {
-            if (c32 < 0xF0) {//1110xxxx 10xxxxxx 10xxxxxx
-                c32 &= 0x0F;
-                c32 <<= 12;
-            }
-            else {//11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
-                c32 &= 0x07;
-                c32 <<= 18;
-                c32 |= (*p++ & 0x3F) << 12;
-            }
-            c32 |= (*p++ & 0x3F) << 6;
-        }
-        c32 |= *p++ & 0x3F;
-    }
-    *ppString = p;
-    return c32;
-}
-
-//pBuffer size should >= 4 bytes
-//caller must input valid utf c32
-inline
-static C *UTF8_Write(C *pBuffer, C32 c32) {
-    C *p = pBuffer;
-    if (c32 < 0x80) { // 0xxxxxxx
-        *p++ = c32;
-        return p;
-    }
-
-    if (c32 < 0x0800) // 110xxxxx 10xxxxxx
-        *p++ = 0xC0 | (c32 >> 6);
-    else {
-        if (c32 < 0x10000) {//1110xxxx 10xxxxxx 10xxxxxx
-            //            assert(c32 < 0xD800 || c32 >= 0xE000);
-            *p++ = 0xE0 | (c32 >> 12);
-        }
-        else {//11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
-            //            assert(c32 < 0x110000);
-            *p++ = 0xF0 | (c32 >> 18);
-            *p++ = 0x80 | ((c32 >> 12) & 0x3F);
-        }
-        *p++ = 0x80 | ((c32 >> 6) & 0x3F);
-    }
-    *p++ = 0x80 | (c32 & 0x3F);
-    return p;
 }
 
 #endif //UTF8_H
