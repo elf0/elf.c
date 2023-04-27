ccd0f32bd2da632db54f970ac2ad2ea0193c7a54
elf0 <elf000@zoho.com>
2023-04-27 15:42:55 +0800

    Modify "StringToU64.h"

--- include/elf/StringToU64.h
+++ include/elf/StringToU64.h
@@ -10,54 +10,45 @@
 //String to U64
 
 //From 'pBegin' to 'pEnd' MUST be a valid number string.
-inline
-    static U64 String_ToU64_2(const C *p, const C *pEnd) {
+inline static U64 String_ToU64_2(const C *p, const C *pEnd) {
     U64 uValue = *p++ - (C)'0';
     while (p != pEnd) {
         uValue <<= 1;
         uValue |= *p++ - (C)'0';
     }
-
     return uValue;
 }
 
-inline
-    static U64 String_ToU64_4(const C *p, const C *pEnd) {
+inline static U64 String_ToU64_4(const C *p, const C *pEnd) {
     U64 uValue = *p++ - (C)'0';
     while (p != pEnd) {
         uValue <<= 2;
         uValue |= *p++ - (C)'0';
     }
-
     return uValue;
 }
 
-inline
-    static U64 String_ToU64_8(const C *p, const C *pEnd) {
+inline static U64 String_ToU64_8(const C *p, const C *pEnd) {
     U64 uValue = *p++ - (C)'0';
     while (p != pEnd) {
         uValue <<= 3;
         uValue |= *p++ - (C)'0';
     }
-
     return uValue;
 }
 
-inline
-    static U64 String_ToU64(const C *p, const C *pEnd) {
+inline static U64 String_ToU64(const C *p, const C *pEnd) {
     U64 uValue = *p++ - (C)'0';
     while (p != pEnd) {
         uValue *= 10;
         uValue += *p++ - (C)'0';
     }
-
     return uValue;
 }
 
-inline
-    static U64 String_ToU64_16(const C *p, const C *pEnd) {
+inline static U64 String_ToU64_16(const C *p, const C *pEnd) {
     U64 uValue = *p++ - (C)'0';
-    if (uValue > 9) {
+    if (uValue >= 10) {
         uValue &= 0xDF;
         uValue -= 0x11;
     }
@@ -65,80 +56,61 @@ inline
     while (p != pEnd) {
         uValue <<= 4;
         U8 u8 = *p++ - '0';
-        if (u8 > 9) {
+        if (u8 >= 10) {
             u8 &= 0xDF;
             u8 -= 0x11;
         }
-
         uValue |= u8;
     }
-
     return uValue;
 }
 
 //Parse '0b' prefix youself
-inline
-    static E8 String_ParseU64_2(U64 *puValue, const C **ppTail) {
-    U64 uValue = *puValue;
+inline static U64 String_ParseU64_2(U64 uHead, const C **ppTail) {
     const C *p = *ppTail;
     U8 uRange;
     while ((uRange = *p - '0') < 2) {
-        if (uValue > 0x7FFFFFFFFFFFFFFF) {
+        if (uHead >= 0x8000000000000000) {
             *ppTail = p;
-            return 1;
+            return 0xFFFFFFFFFFFFFFFF;
         }
-
-        uValue <<= 1;
-        uValue |= uRange;
+        uHead = (uHead << 1) | uRange;
         ++p;
     }
-
     *ppTail = p;
-    *puValue = uValue;
-    return 0;
+    return uHead;
 }
 
-inline
-    static E8 String_ParseU64_4(U64 *puValue, const C **ppTail) {
+inline static U64 String_ParseU64_4(U64 uHead, const C **ppTail) {
     U64 uValue = *puValue;
     const C *p = *ppTail;
     U8 uRange;
     while ((uRange = *p - '0') < 4) {
-        if (uValue > 0x3FFFFFFFFFFFFFFF) {
+        if (uValue >= 0x4000000000000000) {
             *ppTail = p;
-            return 1;
+            return 0xFFFFFFFFFFFFFFFF;
         }
-
-        uValue <<= 2;
-        uValue |= uRange;
+        uHead = (uHead << 2) | uRange;
         ++p;
     }
-
     *ppTail = p;
-    *puValue = uValue;
-    return 0;
+    return uHead;
 }
 
 //Parse '0o' prefix youself
-inline
-    static E8 String_ParseU64_8(U64 *puValue, const C **ppTail) {
-    U64 uValue = *puValue;
+inline static U64 String_ParseU64_8(U64 uHead, const C **ppTail) {
     const C *p = *ppTail;
     U8 uRange;
     while ((uRange = *p - '0') < 8) {
-        if (uValue > 0x1FFFFFFFFFFFFFFF) {
+        if (uHead >= 0x2000000000000000) {
             *ppTail = p;
-            return 1;
+            return 0xFFFFFFFFFFFFFFFF;
         }
-
-        uValue <<= 3;
-        uValue |= uRange;
+        uHead = (uHead << 3) | uRange;
         ++p;
     }
-
     *ppTail = p;
-    *puValue = uValue;
-    return 0;
+    return uHead;
 }
 
 inline static U64 String_ParseU64(U64 uHead, const C **ppTail) {
@@ -160,39 +132,32 @@ inline static U64 String_ParseU64(U64 uHead, const C **ppTail) {
 }
 
 //Parse '0x' prefix youself
-inline
-    static E8 String_ParseU64_16(U64 *puValue, const C **ppTail) {
-    U64 uValue = *puValue;
-    const C *p = *ppTail;
+inline static U64 String_ParseU64_16(U64 uHead, const U8 **ppTail) {
+    const U8 *p = *ppTail;
     while (1) {
         U8 uRange = *p - '0';
-        if (uRange > 9) {
+        if (uRange >= 10) {
             uRange &= 0xDF;
             uRange -= 0x11;
-            if (uRange > 5)
+            if (uRange >= 6)
                 break;
 
             uRange += 10;
         }
 
-        if (uValue > 0x0FFFFFFFFFFFFFFF) {
+        if (uHead >= 0x1000000000000000) {
             *ppTail = p;
-            return 1;
+            return 0xFFFFFFFFFFFFFFFF;
         }
-
-        uValue <<= 4;
-        uValue |= uRange;
+        uHead = (uHead << 4) | uRange;
         ++p;
     }
-
     *ppTail = p;
-    *puValue = uValue;
-    return 0;
+    return uHead;
 }
 
 // return store bits
-inline
-    static U8 U64_Bits(U64 uValue) {
+inline static U8 U64_Bits(U64 uValue) {
     U8 uBits;
     if (uValue > 0xFFFFFFFF) {
         if (uValue > 0xFFFFFFFFFFFF) {
@@ -224,40 +189,35 @@ inline
     return 1;
 }
 
-inline
-    static U8 String_ParseU64Bits_2(U64 uValue, const C **ppTail, U64 *puValue) {
+inline static U8 String_ParseU64Bits_2(U64 uValue, const C **ppTail, U64 *puValue) {
     if (String_ParseU64_2(uValue, ppTail, puValue))
         return 255;
 
     return U64_Bits(*puValue);
 }
 
-inline
-    static U8 String_ParseU64Bits_4(U64 uValue, const C **ppTail, U64 *puValue) {
+inline static U8 String_ParseU64Bits_4(U64 uValue, const C **ppTail, U64 *puValue) {
     if (String_ParseU64_4(uValue, ppTail, puValue))
         return 255;
 
     return U64_Bits(*puValue);
 }
 
-inline
-    static U8 String_ParseU64Bits_8(U64 uValue, const C **ppTail, U64 *puValue) {
+inline static U8 String_ParseU64Bits_8(U64 uValue, const C **ppTail, U64 *puValue) {
     if (String_ParseU64_8(uValue, ppTail, puValue))
         return 255;
 
     return U64_Bits(*puValue);
 }
 
-inline
-    static U8 String_ParseU64Bits_10(U64 uValue, const C **ppTail, U64 *puValue) {
+inline static U8 String_ParseU64Bits_10(U64 uValue, const C **ppTail, U64 *puValue) {
     if (String_ParseU64_10(uValue, ppTail, puValue))
         return 255;
 
     return U64_Bits(*puValue);
 }
 
-inline
-    static U8 String_ParseU64Bits_16(U64 uValue, const C **ppTail, U64 *puValue) {
+inline static U8 String_ParseU64Bits_16(U64 uValue, const C **ppTail, U64 *puValue) {
     if (String_ParseU64_16(uValue, ppTail, puValue))
         return 255;
 
