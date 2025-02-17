f848012ac5fb8871427bb14f117f7d811e274f4a
elf0 <elf000@zoho.com>
2025-02-17 21:53:36 +0800

    Use operator '|' instead of '+' in 'UToS.h'

--- include/elf/UToS.h
+++ include/elf/UToS.h
@@ -1,8 +1,8 @@
 // pEnd - pBegin should be 3
-inline static char *U8ToSD(char *pBegin, char *pEnd, U8 u8) {
+char *U8ToSD(char *pBegin, char *pEnd, U8 u8) {
     char *p = pEnd;
     do
-        *--p = u8 % 10 + '0';
+        *--p = u8 % 10 | '0';
     while (u8 /= 10);
     if (p != pBegin) {
         char *pD = pBegin;
@@ -15,10 +15,10 @@ inline static char *U8ToSD(char *pBegin, char *pEnd, U8 u8) {
 }
 
 // pEnd - pBegin should be 5
-inline static char *U16ToSD(char *pBegin, char *pEnd, U16 u16) {
+char *U16ToSD(char *pBegin, char *pEnd, U16 u16) {
     char *p = pEnd;
     do
-        *--p = u16 % 10 + '0';
+        *--p = (U8)(u16 % 10) | '0';
     while (u16 /= 10);
     if (p != pBegin) {
         char *pD = pBegin;
@@ -31,10 +31,10 @@ inline static char *U16ToSD(char *pBegin, char *pEnd, U16 u16) {
 }
 
 // pEnd - pBegin should be 10
-inline static char *U32ToSD(char *pBegin, char *pEnd, U32 u32) {
+char *U32ToSD(char *pBegin, char *pEnd, U32 u32) {
     char *p = pEnd;
     do
-        *--p = u32 % 10 + '0';
+        *--p = (U8)(u32 % 10) | '0';
     while (u32 /= 10);
     if (p != pBegin) {
         char *pD = pBegin;
@@ -47,10 +47,10 @@ inline static char *U32ToSD(char *pBegin, char *pEnd, U32 u32) {
 }
 
 // pEnd - pBegin should be 20
-inline static char *U64ToSD(char *pBegin, char *pEnd, U64 u64) {
+char *U64ToSD(char *pBegin, char *pEnd, U64 u64) {
     char *p = pEnd;
     do
-        *--p = u64 % 10 + '0';
+        *--p = (U8)(u64 % 10) | '0';
     while (u64 /= 10);
     if (p != pBegin) {
         char *pD = pBegin;
